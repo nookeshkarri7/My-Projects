a75e521c0ebf866aad884542182fd48475ab1652
nookesh <nookeshkarri7@gmail.com>
2021-09-01 21:29:31 +0530

    new class php file added

--- php-projects/lesson2.php
+++ php-projects/lesson2.php
@@ -1,5 +1,15 @@
 <?php 
+    //sngle line comments
     $valuename="NOOKESH"
+    //echo $valuename
+
+    //Multilinecomment
+    /*this
+    is a
+    multiline
+    comment*/
+
+
 ?>
 <html>
     <input type="text" value='<?php echo $valuename ?>' />
