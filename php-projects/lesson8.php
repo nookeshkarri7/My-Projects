7148179f5f1987f341267a1cd06bf921b8bc824a
nookesh <nookeshkarri7@gmail.com>
2021-09-02 08:04:01 +0530

    string manipulations learned

---
+++ php-projects/lesson8.php
@@ -0,0 +1,13 @@
+<?php
+    $a="nookesh is a good.<a href=\"new.html\"> boy</a>";
+    //print_r (str_word_count($a,2,".")); //0,1,2 only step
+    //echo str_shuffle($a);
+    //echo strlen($a)
+    //echo substr($a,0,15);
+
+    //similar_text($a,"b4oy",$res);
+    //echo $res;
+
+    //echo strrev($a);
+    echo $a;
+?>
