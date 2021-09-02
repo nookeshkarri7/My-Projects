f161408546a8cda5bdfbaca04ee9a2dbadece32e
nookesh <nookeshkarri7@gmail.com>
2021-09-02 07:24:19 +0530

    switch statements learned

---
+++ php-projects/lesson6.php
@@ -0,0 +1,17 @@
+<?php
+    $c=551;
+    //Switch statements
+    switch($c){
+        case 1:
+            echo "1";
+            break;
+        case 2:
+            echo "2";
+            break;
+        default:
+            echo $c;
+            break;
+    
+    }
+?>
+
