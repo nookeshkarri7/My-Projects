615a3e95dd6098263170e0d9a50f7c819b4a218b
nookesh <nookeshkarri7@gmail.com>
2021-09-02 07:17:16 +0530

    loops topic learned

---
+++ php-projects/lesson5.php
@@ -0,0 +1,16 @@
+<?php
+    $counter = 1;
+    /*
+    while ($counter <= 10){
+        echo $counter;
+        $counter++;
+    }*/
+    /*do {
+        echo $counter." ";
+        $counter++;
+    }while ($counter<=10);
+    */
+    for ($c=1;$c<=10;$c++){
+        echo $c." ";
+    }
+    ?>
