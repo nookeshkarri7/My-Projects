305c464562db497462e62c3048575e3ccd7bcf3d
nookesh <nookeshkarri7@gmail.com>
2021-09-04 16:02:44 +0530

    counter updated

--- php-projects/pagehitscounter.php
+++ php-projects/pagehitscounter.php
@@ -1,19 +1,14 @@
 <?php
     $name='counter.txt';
-    $size=filesize($name);
+    $size=filesize($name); //no.of.digits in txt file
 
     $file=fopen($name,"r");
     $counter=fread($file,$size);
     fclose($file);
 
     $pcounter=$counter+1;
-
-
     $fileopen=fopen($name,"w");
     fwrite($fileopen,$pcounter);
 
-    
     echo "Page views Count:".$pcounter;
-
-
 ?>
